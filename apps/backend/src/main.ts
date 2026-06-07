8aa314726fe8d74ee537500426b715763d1fc615
Ellen <ellen.andersson89@gmail.com>
2026-06-07 15:44:45 +0200

    Swagger configuration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-   const config = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('Tasks API')
     .setDescription('The tasks API description')
     .setVersion('1.0')
