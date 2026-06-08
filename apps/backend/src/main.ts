a5ca057392bd48e75ebd0916332a1d8ffe507dc3
Alex Wu <itsalexwu@gmail.com>
2026-06-08 16:00:09 +0200

    updated swagger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Tasks API')
+    .setTitle('Household ToDo API')
     .setDescription('The tasks API description')
     .setVersion('1.0')
-    .addTag('tasks')
+    .addTag('Tasks')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
